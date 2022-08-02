16d6d4f8f26790fd30d6b15062364b12043314a9
Egor Moskaliov <58050140+duckbilled395@users.noreply.github.com>
2022-08-02 16:02:19 +0300

    Delete src directory

--- src/shared/hooks/hooks.ts
+++
@@ -1,5 +0,0 @@
-import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
-import {AppDispatch, AppStateType} from "../store";
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<AppStateType> = useSelector;
--- src/shared/store/actions/auth-actions.ts
+++
@@ -1,7 +0,0 @@
-import {AuthActionTypes, AuthDataType, setAuthDataActionType} from "../types/authTypes";
-
-export const setAuthData = (authData: AuthDataType): setAuthDataActionType => ({
-    type: AuthActionTypes.SET_AUTHDATA,
-    payload: authData
-});
-
