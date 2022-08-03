3cf11a18781c18fffc365c873dbcbb132eccc1c9
Egor Moskaliov <58050140+duckbilled395@users.noreply.github.com>
2022-08-03 15:58:36 +0300

    Delete src directory

--- src/shared/store/actions/index.ts
+++
@@ -1,7 +0,0 @@
-import * as AuthActionCreators from './auth-actions'
-import * as PostsPageActionCreators from './posts-page-actions'
-
-export const ActionCreators = {
-    ...AuthActionCreators,
-    ...PostsPageActionCreators
-}
