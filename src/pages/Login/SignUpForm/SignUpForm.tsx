3c324d6a5ff6cf0f3e20d78a15e899a06215b1bc
Egor Moskaliov <58050140+duckbilled395@users.noreply.github.com>
2022-08-03 16:06:57 +0300

    Add files via upload

---
+++ src/pages/Login/SignUpForm/SignUpForm.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Form, Formik } from 'formik'
+import styled from 'styled-components'
+
+import { AuthDataType } from 'shared/store/types/authTypes'
+import { checkBoxLabel } from './lib'
+import { setAuthData } from 'shared/store/actions/auth-actions'
+import validate from './validationSchema'
+import { TextField } from '../TextField/TextField'
+import { CheckBox } from '../CheckBox/CheckBox'
+import { useAppDispatch } from 'shared/hooks/hooks'
+import { useNavigate } from 'react-router-dom'
+
+const StyledLoginForm = styled.div`
+  width: 400px;
+  margin: 50px auto 0 auto;
+`
+const styledForm = {
+	width: '400px'
+}
+const StyledButtonSignUp = styled.button`
+  width: 400px;
+  height: 52px;
+  text-decoration: none;
+  outline: none;
+  margin: 30px 0 0 0;
+`
+
+const SignUpForm: React.FC = () => {
+
+	const dispatch = useAppDispatch()
+	const navigate = useNavigate()
+
+	const handleOnSubmit = (values: AuthDataType) => {
+		dispatch(setAuthData(values))
+		console.log(values)
+		navigate('/posts')
+	}
+
+	return <Formik initialValues={{
+		email: '',
+		password: '',
+		confirmPassword: '',
+		agreements: false
+	}}
+				   validationSchema={validate}
+				   onSubmit={(values) => handleOnSubmit(values)}
+	>
+		{() => (
+			<StyledLoginForm>
+				<Form style={styledForm}>
+					<TextField label='Email' name='email' type='text' />
+					<TextField label='Password' name='password' type='password' />
+					<TextField label='Confirm Password' name='confirmPassword' type='password' />
+					<CheckBox label={checkBoxLabel} name='agreements' type='checkbox' />
+					<StyledButtonSignUp type='submit'>Sign up</StyledButtonSignUp>
+				</Form>
+			</StyledLoginForm>
+		)}
+	</Formik>
+}
+
+export default SignUpForm
